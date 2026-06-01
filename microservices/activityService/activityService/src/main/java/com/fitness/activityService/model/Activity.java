c96639d22378a26ca0f5ebd0abc969c391512fcc
Dipayan Bose <bosedipayan2000@gmail.com>
2026-06-01 17:33:34 +0530

    Added new microservices and modified the same

---
+++ microservices/activityService/activityService/src/main/java/com/fitness/activityService/model/Activity.java
@@ -0,0 +1,36 @@
+package com.fitness.activityService.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Document(collection = "activities")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Activity {
+    private String id;
+    private String userId;
+    private ActivityType type; // e.g., "running", "cycling"
+    private Integer caloriesBurned; // in calories
+    private Integer duration; // in minutes
+    private LocalDateTime startTime; // ISO format date string
+
+    @Field("metrices")
+    private Map<String, Object> additionalMetrices;
+
+    @CreatedDate
+    private LocalDateTime createdAt; // For any extra data specific to the activity type
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
