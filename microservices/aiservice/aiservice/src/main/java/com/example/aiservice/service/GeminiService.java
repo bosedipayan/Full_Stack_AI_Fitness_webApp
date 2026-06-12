c0f6b7955e13d562579d5019dc9b67fda568c481
Dipayan Bose <bosedipayan2000@gmail.com>
2026-06-12 23:07:35 +0530

    Added new microservices and modified existing phases

---
+++ microservices/aiservice/aiservice/src/main/java/com/example/aiservice/service/GeminiService.java
@@ -0,0 +1,51 @@
+package com.example.aiservice.service;
+
+import com.fitness.activityService.config.WebClientConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class GeminiService {
+    private final WebClient webClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public GeminiService(WebClient.Builder webClientConfig) {
+        this.webClient = webClientConfig.build();
+    }
+    public String getRecommendations(String prompt)
+    {
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[] {
+                    Map.of(
+                            "parts", new Object[] {
+                                Map.of(
+                                        "text", prompt
+                                )
+                            }
+                            )
+                }
+        );
+        // Implementation for sending request and receiving response would go here
+
+        String response = webClient.post()
+                .uri(geminiApiUrl)
+                .header("Content-Type", "application/json")
+                .header("Google-api-key", geminiApiKey)
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return response;
+    }
+}
